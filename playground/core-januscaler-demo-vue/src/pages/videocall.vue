c3ad5fdec9c512ed12602d6db9d9612ee11738ab
shivanshtalwar <shivansh@utopia.tech>
2024-08-16 17:16:02 +0300

    fixes and improvements

---
+++ playground/core-januscaler-demo-vue/src/pages/videocall.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+videocall
+    </div>
+</template>
+
+<script setup lang="ts">
+import {JanuScaler}  from '@januscaler/browser-sdk'
+const client=new JanuScaler()
+await client.init({})
+const videoCallHandle=await client.videoCall.createServiceHandle()
+
+</script>
+
+<style scoped>
+
+</style>
